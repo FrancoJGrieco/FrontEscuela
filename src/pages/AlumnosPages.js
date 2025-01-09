0d3da491f5710dfb364f02dec6aabe9087db7b32
Franco <francojesusgrieco@gmail.com>
2025-01-09 10:05:26 -0300

    relacionado con las modificaciones del commit anterior

--- src/pages/AlumnosPages.js
+++ src/pages/AlumnosPages.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect } from 'react'
 import Alumnos from '../components/alumnos/Alumnos'
 import CreateForm from '../components/alumnos/CreateForm'
