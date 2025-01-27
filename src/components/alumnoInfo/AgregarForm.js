03f107acec35af00b95d7b463d92e086d3c6157b
Franco <francojesusgrieco@gmail.com>
2025-01-26 21:49:56 -0300

    Cambios menores

--- src/components/alumnoInfo/AgregarForm.js
+++ src/components/alumnoInfo/AgregarForm.js
@@ -6,29 +6,25 @@ import CloseIcon from '@mui/icons-material/Close'
 export default function AgregarForm () {
   const store = alumnosInfoStore()
   const medidas = { padding: '16px', borderRadius: 10 }
-  return <>
-    {store.notaFormVisibility &&
-      <ModalWindow>
-        <Dialog
-          open={store.notaFormVisibility}
-          slotProps={{
-            paper: {
-              component: 'form',
-              onSubmit: store.agregarNota,
-              style: medidas
-            }
-          }}
-        >
-          <IconButton onClick={store.btnClose} color='primary' edge='start' sx={{ maxWidth: 35, borderRadius: 1, ml: 0 }}>
-            <CloseIcon />
-          </IconButton>
-          <DialogTitle>Nota</DialogTitle>
-          <FormGroup>
-            <TextField name='nota' label='Nota' variant='standard' size='small' margin='dense' onChange={store.handleNotaFieldChange} />
-            <Button type="submit">Crear</Button>
-          </FormGroup>
-        </Dialog>
-      </ModalWindow>
-    }
-  </>
+  return <ModalWindow>
+    <Dialog
+      open={store.notaFormVisibility}
+      slotProps={{
+        paper: {
+          component: 'form',
+          onSubmit: store.agregarNota,
+          style: medidas
+        }
+      }}
+    >
+      <IconButton onClick={store.btnClose} color='primary' edge='start' sx={{ maxWidth: 35, borderRadius: 1, ml: 0 }}>
+        <CloseIcon />
+      </IconButton>
+      <DialogTitle>Nota</DialogTitle>
+      <FormGroup>
+        <TextField name='nota' label='Nota' variant='standard' size='small' margin='dense' onChange={store.handleNotaFieldChange} />
+        <Button type="submit">Crear</Button>
+      </FormGroup>
+    </Dialog>
+  </ModalWindow>
 }
